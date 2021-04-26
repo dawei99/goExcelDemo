9c6b96eb6ed7c92dea805a506792e37d78683707
gaoweihua <811055695@qq.com>
2021-04-26 21:55:44 +0800

    go excel

--- src/excel.go
+++ src/excel.go
@@ -10,12 +10,11 @@ import (
     _ "image/gif"
     _ "image/jpeg"
     _ "image/png"
-    "reflect"
     "flag"
     "regexp"
     "strconv"
 )
-
+var task_now = 0 // 已处理任务个数
 func main () {
 
     var EXCEL_PATH string   // 操作的Excel文件绝对路径
@@ -39,9 +38,11 @@ func main () {
     var addImageParam AddImageParamsStruct  // 需传递参数
     addImageParam.fileHandle = file
     addImageParam.firstSheetName = firstSheetName
-
-    addImageChannel := make(chan AddImageParamsStruct) // 添加图像操作 管道
-    go addImage(addImageChannel) // 添加图像逻辑函数
+    finishNoticeHandle := make(chan int) // 全部完成通知管道
+    total := 0 // 任务总数
+    jsonparser.ArrayEach([]byte(IMGS_JSON), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+        total++;
+    })
     // 图像添加处理
     jsonparser.ArrayEach([]byte(IMGS_JSON), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
         pathByte,_,_,errPath:=jsonparser.Get(value, "path") ; path := string(pathByte) // 图片绝对路径
@@ -56,64 +57,60 @@ func main () {
         addImageParam.pos = pos
         addImageParam.height = height
         addImageParam.width = width
-        addImageChannel<-addImageParam
+        addImageParam.total_number = total
+        go addImage(addImageParam, finishNoticeHandle)
     })
-    // 任务完成
-    var finish AddImageParamsStruct ; finish.status = 1 ; addImageChannel<-finish
-    c := <-addImageChannel
-    if c.status == 1 {
-        // 保存图像
-        errSave := file.Save()
-        throwErr(errSave, "Excel文件保存失败!") // 检测打开失败
-    }
-    reflect.TypeOf(10)
+    <-finishNoticeHandle // 等待任务完成
+
+    // 保存图像
+    errSave := file.Save()
+    throwErr(errSave, "Excel文件保存失败!") // 检测打开失败
+
 }
 
 // excel添加图片操作
-func addImage(c chan AddImageParamsStruct){
-    for {
-        info := <-c
-        // 任务全部完成
-        if info.status == 1 { break }
-        width := info.width; pos := info.pos; height := info.height; path := info.path // 宽度、位置、高度、图片地址
-        firstSheetName := info.firstSheetName  // 工作薄名称
-        fileHandle := info.fileHandle  // excel操作句柄
-        rowMatch := regexp.MustCompile(`[a-zA-Z]+([0-9]+)`).FindAllStringSubmatch(pos, -1) // 行数
-        colMatch := regexp.MustCompile(`([a-zA-Z])+[0-9]+`).FindAllStringSubmatch(pos, -1) // 列数
+func addImage(info AddImageParamsStruct, finishNotice chan int) (string, error) {
+    defer (func() {
+        task_now++
+        if task_now == info.total_number {
+            finishNotice<-1
+        }
+    })()
 
-        // 添加图片
-        if errAddPic := fileHandle.AddPicture(firstSheetName, pos, path, `{ "x_scale": 1,"y_scale": 1,"positioning": "absolute","x_offset": 1,"y_offset": 1}`) ; errAddPic != nil {
-            fmt.Println("图片添加失败", path, pos)
-            fmt.Println(errAddPic)
-            continue
-        }
-        // 设置行高
-        rowNumString := rowMatch[0][1]
-        rowNum, errRowNum := strconv.Atoi(rowNumString) // 行号
-        heightNum, errHeightNum := strconv.ParseFloat(height, 64) // 行高
-        if errRowNum == nil && errHeightNum == nil {
-            fileHandle.SetRowHeight(firstSheetName, rowNum, heightNum)
-        } else {
-            fmt.Println("行高设置失败", rowNum, heightNum)
-            continue
-        }
-        // 设置列宽
-        colNum := colMatch[0][1] // 列号
-        if widthNum, errWidthNum := strconv.ParseFloat(width, 64) ; errWidthNum == nil {
-            fileHandle.SetColWidth(firstSheetName, colNum, colNum, widthNum)
-        } else {
-            if len(width) != 0 {
-                fmt.Println("列宽设置失败：", pos, colNum, width)
-            }
-            continue
+    width := info.width; pos := info.pos; height := info.height; path := info.path // 宽度、位置、高度、图片地址
+    firstSheetName := info.firstSheetName  // 工作薄名称
+    fileHandle := info.fileHandle  // excel操作句柄
+    rowMatch := regexp.MustCompile(`[a-zA-Z]+([0-9]+)`).FindAllStringSubmatch(pos, -1) // 行数
+    colMatch := regexp.MustCompile(`([a-zA-Z])+[0-9]+`).FindAllStringSubmatch(pos, -1) // 列数
+
+    // 添加图片
+    if errAddPic := fileHandle.AddPicture(firstSheetName, pos, path, `{ "x_scale": 1,"y_scale": 1,"positioning": "absolute","x_offset": 1,"y_offset": 1}`) ; errAddPic != nil {
+        return "",&myError{"图片添加失败，"+"path："+path+"，pos："+pos}
+    }
+    // 设置行高
+    rowNumString := rowMatch[0][1]
+    rowNum, errRowNum := strconv.Atoi(rowNumString) // 行号
+    heightNum, errHeightNum := strconv.ParseFloat(height, 64) // 行高
+    if errRowNum == nil && errHeightNum == nil {
+        fileHandle.SetRowHeight(firstSheetName, rowNum, heightNum)
+    } else {
+        return "",&myError{"行高设置失败，"+"rowNum："+strconv.Itoa(rowNum)+"，heightNum："+strconv.FormatFloat(heightNum, 'E', -1, 64)}
+    }
+    // 设置列宽
+    colNum := colMatch[0][1] // 列号
+    if widthNum, errWidthNum := strconv.ParseFloat(width, 64) ; errWidthNum == nil {
+        fileHandle.SetColWidth(firstSheetName, colNum, colNum, widthNum)
+    } else {
+        if len(width) != 0 {
+            return "",&myError{"行宽设置失败，pos："+pos+"，colNum："+colNum+"，width："+width}
         }
     }
-    var finish AddImageParamsStruct ; finish.status = 1 ; c<-finish
+    return "完成", nil
 }
 
 // 管道信息
 type BaseExcelInfoStruct struct {
-    status int  // 任务处理状态 0:未完成(默认) 1:完成
+    total_number int  // 任务总数
 }
 
 // excel文件信息 结构体
@@ -139,4 +136,13 @@ func throwErr(err error, msg string){
         fmt.Println(err)
         os.Exit(1)
     }
+}
+
+// 错误抛出
+type myError struct {
+    message string
+}
+
+func (e *myError) Error () string {
+    return e.message
 }
